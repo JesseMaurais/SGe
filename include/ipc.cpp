c0a9a04d470ab785e74461e4ffb699f5fd16cea2
Jesse Maurais <jessemaurais@gmail.com>
2018-07-22 11:23:48 -0600

    Yet more changes to ipc stream

--- include/ipc.cpp
+++ include/ipc.cpp
@@ -1,64 +1,29 @@
 #include "ipc.hpp"
 #include "unisys.hpp"
 
-namespace
-{
-	pid_t spork(int fd[2])
-	{
-		if (-1 == pipe(fd))
-		{
-			return -1;
-		}
-		pid_t pid = fork();
-		if (-1 == pid)
-		{
-			close(fd[0]);
-			close(fd[1]);
-		}
-		return pid;
-	}
-}
-
 namespace io
 {
-	template <class Char, template <class> class Traits>
-	basic_iipcstream<Char, Traits>::basic_iipcstream()
+	template
+	<
+	 class Char,
+	 template <class> class Traits,
+	 template <class> class Alloc
+	>
+	void basic_pstream::open(string_view program, openmode mode)
 	{
-		int fd[2];
-		pid = spork(fd);
-		if (-1 == pid)
-		{
-			// error
-		}
-		else
-		if (0 == pid)
-		{
-			if (-1 == dup2(fd[1], STDOUT_FILENO))
-			{
-				// error
-			}
-			if (-1 == close(fd[0]))
-			{
-				// error
-			}
-		}
-		else
-		{
-			if (-1 == close(fd[1]))
-			{
-				// error
-			}
-			base::setfd(fd[0]);
-		}
+	}
+
+	template
+	<
+	 class Char,
+	 template <class> class Traits,
+	 template <class> class Alloc
+	>
+	void basic_pstream::close()
+	{
 	}
 	
-	template class basic_iipstream<char>;
-	template class basic_iipstream<wchar_t>;
-
-	template class basic_oipstream<char>;
-	template class basic_oipstream<wchar_t>;
-
-	template class basic_ipstream<char>;
-	template class basic_ipstream<wchar_t>;
+	template class basic_pstream<char>;
+	template class basic_pstream<wchar_t>;
 }
 
--- include/ipc.hpp
+++ include/ipc.hpp
@@ -7,47 +7,94 @@
 
 namespace sys::io
 {
-	template <class Char, template <class> class Traits = std::char_traits>
-	class basic_ipstream : public basic_ifdstream<Char, Traits>
+	template
+	<
+	 class Char,
+	 template <class> class Traits = std::char_traits,
+	 template <class> class Alloc = std::allocator
+	>
+	class basic_pstream : public basic_fdstream<Char, Traits, Alloc>
+	{
+		using traits = typename Traits<Alloc>;
+		using string_view = typename basic_string_view<Char, traits>;
+		using namespace std::ios_base;
+
+	public:
+
+		basic_pstream();
+		basic_pstream(string_view program, openmode mode = in|out)
+		{
+			open(program, mode);
+		}
+		~basic_pstream()
+		{
+			close();
+		}
+		
+		bool is_open() const;
+		virtual void open(string_view program, openmode mode = in|out);
+		void close();
+	};
+
+	using pstream = basic_pstream<char>;
+	using wpstream = basic_pstream<wchar_t>;
+
+	extern template basic_pstream<char>;
+	extern template basic_pstream<wchar_t>;
+
+	template
+	<
+	 class Char,
+	 template <class> class Traits = std::char_traits,
+	 template <class> class Alloc = std::allocator
+	>
+	class basic_ipstream : public basic_pstream<Char, Traits, Alloc>
 	{
 	public:
 
 		basic_ipstream();
-	};
+		basic_ipstream(string_view program, openmode mode = in)
+		: basic_pstream(program mode)
+		{ }
 
-	template <class Char, template <class> Traits = std::char_traits>
-	class basic_opstream : public basic_ofdstream<Char, Traits>
-	{
-	public:
-
-		basic_opstream();
-	};
-
-	template <class Char, template <class> Traits = std::char_traits>
-	class basic_pstream : public basic_fdstream<Char, Traits>
-	{
-	public:
-
-		basic_pstream();
+		void open(string_view program, openmode mode = in) override
+		{
+			basic_pstream::open(program, mode);
+		}
 	};
 	
 	using ipstream = basic_ipstream<char>;
 	using wipstream = basic_ipstream<wchar_t>;
 
-	using opstream = basic_opstream<char>;
-	using wopstream = basic_opstream<wchar_t>;
-
-	using pstream = basic_pstream<char>;
-	using wpstream = basic_pstream<wchar_t>;
-
 	extern template basic_ipstream<char>;
 	extern template basic_ipstream<wchar_t>;
 
+	template 
+	<
+	 class Char,
+	 template <class> class Traits = std::char_traits,
+	 template <class> class Alloc = std::allocator
+	>
+	class basic_opstream : public basic_pstream<Char, Traits, Alloc>
+	{
+	public:
+
+		basic_opstream();
+		basic_opstream(string_view program, openmode mode = out)
+		: basic_pstream(program, mode)
+		{ }
+
+		void open(string_view program, openmode mode = out) override
+		{
+			basic_pstream::open(program, mode);
+		}
+	};
+
+	using opstream = basic_opstream<char>;
+	using wopstream = basic_opstream<wchar_t>;
+
 	extern template basic_opstream<char>;
 	extern template basic_opstream<wchar_t>;
-
-	extern template basic_pstream<char>;
-	extern template basic_pstream<wchar_t>;
 }
 
 #endif // file
